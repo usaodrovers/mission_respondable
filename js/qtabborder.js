56c06703c826dab42f39d3f967e746020e338272
Cody Dracars <cdracars@gmail.com>
2013-11-25 11:14:16 -0600

    SMACSS on quicktab

--- js/qtabborder.js
+++ js/qtabborder.js
@@ -1,7 +1,7 @@
 (function ($) {
      Drupal.behaviors.qtabborder = {
              attach: function (context, settings) {
-             $('.view-id-featured_testimonials li a').click(
+             $('.quicktab-block li a').click(
                function () {
                 console.log("Fired");
                 $(this).parent().siblings().find('.hide-border').removeClass('hide-border');
