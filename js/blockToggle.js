eb7bbf32b5901749e9331519fc3bf491a416c456
Staris <jaison@worxco.com>
2014-01-13 11:29:06 -0600

    Future optimized the code.
    Signed-off-by: Staris <jaison@worxco.com>

--- js/blockToggle.js
+++ js/blockToggle.js
@@ -3,16 +3,11 @@
     attach: function (context, settings) {
       if ($(".l-sidebar-left").css("float") != "left"){
         $(".l-box__toggle ul.menu__vertical").hide();
+        $(".l-box__toggle div.view").hide();
         $(".l-box__toggle h2").click(
           function () {
             console.log("Hello");
             $(".l-box__toggle ul.menu__vertical").toggle();
-          }
-        );
-        $(".l-box__toggle div.view").hide();
-        $(".l-box__toggle h2").click(
-          function () {
-            console.log("Hello 2");
             $(".l-box__toggle div.view").toggle();
           }
         );
