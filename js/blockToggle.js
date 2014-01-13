f0fdaedf0703c900b3f4a6e9246a55de0f859a66
Staris <jaison@worxco.com>
2014-01-13 10:39:21 -0600

    Add the java script that should handle hiding at the mobile level for the rest of the content.
    Signed-off-by: Staris <jaison@worxco.com>

--- js/blockToggle.js
+++ js/blockToggle.js
@@ -9,10 +9,20 @@
             $(".l-box__toggle ul.menu__vertical").toggle();
           }
         );
+        $(".l-box__toggle div.view").hide();
+        $(".l-box__toggle h2").click(
+          function () {
+            console.log("Hello 2");
+            $(".l-box__toggle div.view").toggle();
+          }
+        );
       }
 
       if ($(".l-sidebar-left").css("float") == "left"){
         $(".l-box__toggle ul.menu__vertical").show();
+      }
+      if ($(".l-sidebar-left").css("float") == "left"){
+        $(".l-box__toggle div.view").show();
       }
     }
   }
