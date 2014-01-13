95271579ba4e5c8c1e5d77cf48eea10fb1e3c013
Staris <jaison@worxco.com>
2014-01-13 11:07:35 -0600

    Fixed the show since the if was the same.
    Signed-off-by: Staris <jaison@worxco.com>

--- js/blockToggle.js
+++ js/blockToggle.js
@@ -20,8 +20,6 @@
 
       if ($(".l-sidebar-left").css("float") == "left"){
         $(".l-box__toggle ul.menu__vertical").show();
-      }
-      if ($(".l-sidebar-left").css("float") == "left"){
         $(".l-box__toggle div.view").show();
       }
     }
