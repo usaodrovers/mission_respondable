3fb1d51ad18e0a385979e3ac3f10c7e5191e8821
Cody Dracars <cdracars@gmail.com>
2014-03-14 15:49:07 -0500

    Updated js file to new classes and to account for 3 column layout. Updated general css for new relative position. Added new partial for classes used in js file. Add import statement to mission_respondable.scss for new partial

--- js/alignmentShift.js
+++ js/alignmentShift.js
@@ -1,35 +1,95 @@
 jQuery(window).load(function (){
   var lPos = 0;
   var rPos = 0;
+  var lcPos = 0
+  var ccPos = 0
+  var rcPos = 0
   if (screen.width > 850){
-    jQuery('.panel-display .l-sidebar-left').each(function(i, j){
-      worx_resizer_l(i, j);
+    jQuery('.panel-flexible .l-sidebar-left').each(function(i, j){
+      var side = 'left';
+      worx_resizer(i, j, side);
     });
-    jQuery('.panel-display .l-content-right').each(function(i, j){
-      worx_resizer_r(i, j);
+    jQuery('.panel-flexible .l-content-right').each(function(i, j){
+      var side = 'right';
+      worx_resizer(i, j, side);
+    });
+    jQuery('.panel-flexible .l-3col-left').each(function(i, j){
+      var side = 'leftc';
+      worx_resizer(i, j, side);
+    });
+    jQuery('.panel-flexible .l-3col-center').each(function(i, j){
+      var side = 'centerc';
+      worx_resizer(i, j, side);
+    });
+    jQuery('.panel-flexible .l-3col-right').each(function(i, j){
+      var side = 'rightc';
+      worx_resizer(i, j, side);
     });
   }
-  function worx_resizer_l(i, j) {
+  function worx_resizer(i, j, side) {
     var dHeight = jQuery(j).height();
-    if (i) {
-      jQuery(j).css("position", "absolute");
-      jQuery(j).css("top", lPos);
+    if (side == 'left') {
+      if (i) {
+        jQuery(j).addClass("ashift-pos")
+        jQuery(j).css("top", lPos);
+      }
+      lPos = lPos + dHeight + 15;
     }
-    lPos = lPos + dHeight + 15;
+    else if (side == 'right') {
+      if (i) {
+        jQuery(j).addClass("ashift-pos")
+        jQuery(j).addClass("ashift-right")
+        jQuery(j).css("top", rPos);
+      }
+      rPos = rPos + dHeight + 15;
+    }
+    else if (side == 'leftc') {
+      if (i) {
+        jQuery(j).addClass("ashift-pos")
+        jQuery(j).css("top", lcPos);
+      }
+      lcPos = lcPos + dHeight + 15;
+    }
+    else if (side == 'centerc') {
+      if (i) {
+        jQuery(j).addClass("ashift-pos")
+        jQuery(j).css("top", ccPos);
+      }
+      ccPos = ccPos + dHeight + 15;
+    }
+    else if (side == 'rightc') {
+      if (i) {
+        jQuery(j).addClass("ashift-pos")
+        jQuery(j).css("top", rcPos);
+      }
+      rcPos = rcPos + dHeight + 15;
+    }
+    var column = worx_bigger(lcPos, ccPos, rcPos);
+    worx_footer_fix(lPos, rPos, column);
   }
-  function worx_resizer_r(i, j) {
-    var dHeight = jQuery(j).height();
-    if (i) {
-      jQuery(j).css("position", "absolute");
-      jQuery(j).css("top", rPos);
-      jQuery(j).css("left", "100%");
+  function worx_bigger(lcPos, ccPos, rcPos) {
+    if (lcPos > ccPos && lcPos > rcPos) {
+      return lcPos;
     }
-    rPos = rPos + dHeight + 15;
-    if (lPos >= rPos) {
-      jQuery("#main").height(lPos + 100);
+    else if (ccPos > rcPos && ccPos > lcPos) {
+      return ccPos;
     }
     else {
-      jQuery("#main").height(rPos + 100);
+      return rcPos;
+    }
+  }
+  function worx_footer_fix(lPos, rPos, column) {
+    if (column > 0) {
+      var top = column;
+    }
+    else {
+      var top = jQuery('.p-box-full').height();
+    }
+    if (lPos >= rPos) {
+      jQuery("#main").height(top + lPos + 100);
+    }
+    else {
+      jQuery("#main").height(top + rPos + 100);
     }
   }
 });
