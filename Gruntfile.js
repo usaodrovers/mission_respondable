43438c19d20caf41048ac4c4006ce79e20a51852
Cody Dracars <cdracars@gmail.com>
2014-05-29 15:04:51 -0500

    setup grunt linting and linted accordian.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,100 @@
+/*!
+ * Mission Repondable Theme Gruntfile
+ * http://usao.edu
+ * @author Cody Dracars
+ */
+
+'use strict';
+
+/*
+ * Grunt Module
+ */
 module.exports = function(grunt) {
  
-  // Project configuration.
+  /*
+   * Configuration
+   */
   grunt.initConfig({
-    // This line makes your node configurations available for use
+
+    /*
+     * Get package meta data.
+     */
     pkg: grunt.file.readJSON('package.json'),
-    // This is where we configure JSHint
+
+    /*
+     * Set project object.
+     */
+    project: {
+      src: [
+        '/var/www/drupal/profiles/d7_usao_edu/themes/mission_respondable'
+      ],
+      css: [
+        '<%= project.src %>/sass/mission_respondable.scss'
+      ],
+      js: [
+        '<%= project.src%>/js/*.js'
+      ]
+    },
+
+    /*
+     * jshint
+     */
     jshint: {
       // You get to make the name
       // The paths tell JSHint which files to validate
-      myFiles: ['js/*.js']
+      myFiles: ['<%= project.js %>']
+    },
+    
+    /*
+     * sass-lint
+     */
+     scsslint: {
+       allFiles: [
+         '<%= project.src %>/sass/mission_respondable.scss',
+         '<%= project.src %>/sass/partials/*.scss',
+       ],
+       options: {
+         bundleExec: false,
+         config: '.scss-lint.yml',
+         reporterOutput: 'scss-lint-report.xml',
+         colorizeOutput: true
+       },
+     },
+
+     /**
+     * Compass
+     */
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css'
+        }
+      }
+    }, 
+
+    /**
+     * Watch
+     */
+    watch: {
+      css: {
+        files: '<%= project.src %>/scss/{,*/}*.{scss,sass}',
+        tasks: ['compass']
+      }
     }
   });
-  // Each plugin must be loaded following this pattern
-  grunt.loadNpmTasks('grunt-contrib-jshint');
- 
+
+  /**
+   * Load Grunt plugins
+   */
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  /**
+   * Default task
+   * Run `grunt` on the command line
+   */
+  grunt.registerTask('default', [
+    'compass',
+    'watch'
+  ]);
 };
