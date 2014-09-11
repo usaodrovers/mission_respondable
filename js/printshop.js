536f4aebd3a08e5aad4f090c11e584333d3febe9
cdracars <cdracars@gmail.com>
2014-09-11 14:32:21 -0500

    Switch ID to Class

--- js/printshop.js
+++ js/printshop.js
@@ -2,8 +2,8 @@
   Drupal.behaviors.printshop = {
     attach: function (context, settings) {
       $('#edit-submitted-ncr-forms').click(function () {
-        if ($('#webform-component--ncr-form-information').is('.collapsed')) {
-          Drupal.toggleFieldset('#webform-component--ncr-form-information');
+        if ($('.webform-component--ncr-form-information').is('.collapsed')) {
+          Drupal.toggleFieldset('.webform-component--ncr-form-information');
         }
       });
     }
