882c4e9a9a76e2217cc4cd6ff040a97c0246a0cf
cdracars <cdracars@gmail.com>
2014-09-12 14:30:21 -0500

    Add remove class

--- js/printshop.js
+++ js/printshop.js
@@ -5,6 +5,7 @@
       $('#edit-submitted-ncr-forms').click(function () {
         if ($('.webform-component--ncr-forms-information').is('.collapsed')) {
           $('.webform-component--ncr-forms-information').toggle();
+          $('.webform-component--ncr-forms-information').removeClass('collapsed');
         }
       });
     }
