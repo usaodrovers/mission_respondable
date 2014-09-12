54c6f5db1d217b1b23060c62739c1daae47bdea2
cdracars <cdracars@gmail.com>
2014-09-12 14:09:19 -0500

    Set printshop ncr form to toggle

--- js/printshop.js
+++ js/printshop.js
@@ -1,9 +1,11 @@
 (function ($) {
   Drupal.behaviors.printshop = {
     attach: function (context, settings) {
+      $('.webform-component--ncr-form-information').addClass('collapsed');
       $('#edit-submitted-ncr-forms').click(function () {
         if ($('.webform-component--ncr-form-information').is('.collapsed')) {
           Drupal.toggleFieldset('.webform-component--ncr-form-information');
+          $('.webform-component--ncr-form-information').removeClass('collapsed');
         }
       });
     }
