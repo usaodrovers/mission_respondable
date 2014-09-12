d533cccfb9df65075e5b29fc5f6f32fbb9587a9a
cdracars <cdracars@gmail.com>
2014-09-12 14:19:53 -0500

    Stupid typo

--- js/printshop.js
+++ js/printshop.js
@@ -1,9 +1,9 @@
 (function ($) {
   Drupal.behaviors.printshop = {
     attach: function (context, settings) {
-      $('.webform-component--ncr-form-information').addClass('collapsed');
+      $('.webform-component--ncr-forms-information').addClass('collapsed');
       $('#edit-submitted-ncr-forms').click(function () {
-        if ($('.webform-component--ncr-form-information').is('.collapsed')) {
+        if ($('.webform-component--ncr-forms-information').is('.collapsed')) {
           Drupal.toggleFieldset('.webform-component--ncr-form-information');
           $('.webform-component--ncr-form-information').removeClass('collapsed');
         }
