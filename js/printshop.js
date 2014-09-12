2af754f7cfdab925b36ecbbd9d24fca5fc465a0b
cdracars <cdracars@gmail.com>
2014-09-12 14:22:57 -0500

    Stupid typo

--- js/printshop.js
+++ js/printshop.js
@@ -4,8 +4,7 @@
       $('.webform-component--ncr-forms-information').addClass('collapsed');
       $('#edit-submitted-ncr-forms').click(function () {
         if ($('.webform-component--ncr-forms-information').is('.collapsed')) {
-          Drupal.toggleFieldset('.webform-component--ncr-form-information');
-          $('.webform-component--ncr-form-information').removeClass('collapsed');
+          Drupal.toggleFieldset('.webform-component--ncr-forms-information');
         }
       });
     }
