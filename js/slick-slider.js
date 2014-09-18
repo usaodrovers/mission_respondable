12e02dd2fae4f5c41c08fcf2ae3b6befb1cc7c74
Cody Dracars <cdracars@gmail.com>
2014-09-18 09:49:39 -0500

    Work on header and made slider work with 1.7

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -1,12 +1,13 @@
+(function($) {
 var count = 0;
-jQuery(document).ready(function() {
+$(document).ready(function() {
   // Fire in initial resize function on page load so slides appear sized
   // correctly
   measure_slide_fast();
 
   // Initialize the slick slider function.
   // find properties and documentation here: http://kenwheeler.github.io/slick/
-  jQuery('.pane-gallery-image-header-nesbitt-gallery-slider .view-content').slick({
+  $('.pane-gallery-image-header-nesbitt-gallery-slider .view-content').slick({
     centerMode: true,
     centerPadding: '23%',
     slidesToShow: 1,
@@ -30,7 +31,7 @@ jQuery(document).ready(function() {
   // Check height of div containing slides because occasionaly the script loads
   // before the images load. 
   function measure_slide_fast() {
-    var height = jQuery('.slick-list').height();
+    var height = $('.slick-list').height();
     if (height > 440 || height < 30) {
       setTimeout(measure_slide_fast, 100);
     }
@@ -45,8 +46,8 @@ jQuery(document).ready(function() {
     var margin = Math.floor((baseHeight - sideSlide) / 2);
 
     // Set smaller slide heights and margins based on vars calculated earlier.
-    jQuery('.view-display-id-nesbitt_gallery_slider .slick-slide').css('max-height', sideSlide);
-    jQuery('.view-display-id-nesbitt_gallery_slider .slick-slide').css('margin-top', margin);
+    $('.view-display-id-nesbitt_gallery_slider .slick-slide').css('max-height', sideSlide);
+    $('.view-display-id-nesbitt_gallery_slider .slick-slide').css('margin-top', margin);
 
     // Now set height of center slide. Which slide is 'center' depends on where
     // in page load we are. If the page is initially loaded the .slick-center
@@ -54,19 +55,20 @@ jQuery(document).ready(function() {
     // class is applied to the proper slide, so we find the center and apply
     // the correct height to the next slide in line
     if (count) {
-      jQuery('.view-display-id-nesbitt_gallery_slider .slick-slide.slick-center').next().css('max-height', baseHeight);
-      jQuery('.view-display-id-nesbitt_gallery_slider .slick-slide.slick-center').next().css('margin-top', '0');
+      $('.view-display-id-nesbitt_gallery_slider .slick-slide.slick-center').next().css('max-height', baseHeight);
+      $('.view-display-id-nesbitt_gallery_slider .slick-slide.slick-center').next().css('margin-top', '0');
     }
     else {
-      jQuery('.view-display-id-nesbitt_gallery_slider .slick-slide.slick-center').css('max-height', baseHeight);
-      jQuery('.view-display-id-nesbitt_gallery_slider .slick-slide.slick-center').css('margin-top', '0');
+      $('.view-display-id-nesbitt_gallery_slider .slick-slide.slick-center').css('max-height', baseHeight);
+      $('.view-display-id-nesbitt_gallery_slider .slick-slide.slick-center').css('margin-top', '0');
     }
     count++;
   }
 
-  jQuery(window).resize(function() {
-    // var screenWidth = jQuery(window).width();
+  $(window).resize(function() {
+    // var screenWidth = $(window).width();
     count = 0;
     measure_slide_fast();
   });
 });
+})(jq17)
