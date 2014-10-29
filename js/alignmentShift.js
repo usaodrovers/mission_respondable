ebdb787a88a39e6a105450049094235a057bbf35
cdracars <cdracars@gmail.com>
2014-10-29 14:32:47 -0500

    Put alignment back right

--- js/alignmentShift.js
+++ js/alignmentShift.js
@@ -4,22 +4,8 @@ jQuery(window).load(function (e) {
   var lcPos = 0;
   var ccPos = 0;
   var rcPos = 0;
-  var tourHeight = jQuery('.vtour').height();
-  console.log(tourHeight); //Just to verify it exists.
-  
-  resize_init();
-  
-  function resize_init() {
-    if (jQuery(window).width() > 481){
-      if (tourHeight > 40) { 
-        worx_resize_select();
-      }
-      else {
-        setTimeout(function() {
-          resize_init();
-        }, 100);
-      }
-    }
+  if (jQuery(window).width() > 481){
+    worx_resize_select();
   }
   function worx_resize_select() {
     lPos = 0;
