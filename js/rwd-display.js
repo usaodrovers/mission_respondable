e4ff2e6f6b610079fe182553c8e3f20fe4d32a68
cdracars <cdracars@gmail.com>
2014-10-29 22:10:26 -0500

    Fixed spacing

--- js/rwd-display.js
+++ js/rwd-display.js
@@ -1,43 +1,43 @@
 (function ($) {
   Drupal.behaviors.rwd_display = {
     attach: function (context, settings) {
-// viewport stuff
-var targetWidth = 980;
-var deviceWidth = 'device-width';
-var viewport = $('meta[name="viewport"]');
+        // viewport stuff
+        var targetWidth = 980;
+        var deviceWidth = 'device-width';
+        var viewport = $('meta[name="viewport"]');
 
-// check to see if local storage value is set on page load
-localStorage.isResponsive = (localStorage.isResponsive == undefined) ? 'true' : localStorage.isResponsive;
+        // check to see if local storage value is set on page load
+        localStorage.isResponsive = (localStorage.isResponsive == undefined) ? 'true' : localStorage.isResponsive;
 
-var showFullSite = function(){    
-    viewport.attr('content', 'width=' + targetWidth);  
-    
-    if(!$('.rwd-display-options #view-responsive').length){
-        $('.rwd-display-options').append('<span id="view-responsive">View Mobile Optimized</span>');
-    }    
-    
-    localStorage.isResponsive = 'false';
-}
+        var showFullSite = function(){
+            viewport.attr('content', 'width=' + targetWidth);
 
-var showMobileOptimized = function(){
-    localStorage.isResponsive = 'true';
-    viewport.attr('content', 'width=' + deviceWidth);
-}
+            if(!$('.rwd-display-options #view-responsive').length){
+                $('.rwd-display-options').append('<span id="view-responsive">View Mobile Optimized</span>');
+            }
 
-// if the user previously chose to view full site, change the viewport
-if(Modernizr.localstorage){
-    if(localStorage.isResponsive == 'false'){
-        showFullSite();
-    }
-}    
+            localStorage.isResponsive = 'false';
+        }
 
-$("#view-full").on("click", function(){
-    showFullSite();
-});
+        var showMobileOptimized = function(){
+            localStorage.isResponsive = 'true';
+            viewport.attr('content', 'width=' + deviceWidth);
+        }
 
-$('.rwd-display-options').on("click", "#view-responsive", function(){
-    showMobileOptimized();
-});
+        // if the user previously chose to view full site, change the viewport
+        if(Modernizr.localstorage){
+            if(localStorage.isResponsive == 'false'){
+                showFullSite();
+            }
+        }
+
+        $("#view-full").on("click", function(){
+            showFullSite();
+        });
+
+        $('.rwd-display-options').on("click", "#view-responsive", function(){
+            showMobileOptimized();
+        });
      }
   };
 })(jQuery);
