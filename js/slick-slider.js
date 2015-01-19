7654c41b7b472d8844a02de1c5bcf04ebb1d5217
Aegir user <aegir@usao-d7.usao.edu>
2015-01-19 10:58:56 -0600

    Stoped autoplay

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -12,7 +12,7 @@
         centerMode: true,
         centerPadding: '23%',
         slidesToShow: 1,
-        autoplay: true,
+        autoplay: false,
         speed: 700,
         // Can't pass params in onBeforeChange setting function. boo.
         onBeforeChange: measure_slide_fast,
