862b26a29d15843dd3cbd56913781c71e39c798e
cdracars <cdracars@gmail.com>
2015-02-20 16:20:31 -0600

    compressed as default for css output

--- config.rb
+++ config.rb
@@ -50,7 +50,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+output_style = (environment == :compressed) ? :expanded : :compressed
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
